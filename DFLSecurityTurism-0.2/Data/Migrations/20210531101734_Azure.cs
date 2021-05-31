2387366b375df96ed19ffda1c7d82d6fcc6569df
LUIS CARDOSO <iscac16274@alumni.iscac.pt>
2021-05-31 13:43:03 +0100

    Update

---
+++ DFLSecurityTurism-0.2/Data/Migrations/20210531101734_Azure.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DFLSecurityTurism_0._2.Data.Migrations
+{
+    public partial class Azure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descrição5",
+                table: "Restaurante",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descrição5",
+                table: "Hotel",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descrição5",
+                table: "Restaurante");
+
+            migrationBuilder.DropColumn(
+                name: "Descrição5",
+                table: "Hotel");
+        }
+    }
+}
